d84bbf229b5cc73bd5476cf1ac63151e62602934
Dmitry Batseko <studytemp@hotmail.com>
2020-04-19 23:51:08 +0300

    Fix ToggleUserBan after exception set previous ban status

--- src/main/java/by/batseko/library/command/reciever/admin/ToggleUserBan.java
+++ src/main/java/by/batseko/library/command/reciever/admin/ToggleUserBan.java
@@ -16,14 +16,23 @@ public class ToggleUserBan implements Command {
     public Router execute(HttpServletRequest request, HttpServletResponse response) {
         Router currentRouter = new Router();
         String redirectCommand = request.getParameter(JSPAttributeStorage.REDIRECT_PAGE_COMMAND);
+        int userId = Integer.parseInt(request.getParameter(JSPAttributeStorage.USER_ID));
+        User user;
         try {
-            int userId = Integer.parseInt(request.getParameter(JSPAttributeStorage.USER_ID));
-            User user = userService.findUserById(userId);
+            user = userService.findUserById(userId);
+        } catch (LibraryServiceException e) {
+            setErrorMessage(request, e.getMessage());
+            currentRouter.setPagePath(PageStorage.HOME);
+            currentRouter.setRouteType(Router.RouteType.FORWARD);
+            return currentRouter;
+        }
+        try {
             user.setBanned(getReversedUserBannedStatus(user.getBanned()));
             userService.updateUserBanStatus(user);
             currentRouter.setPagePath(redirectCommand);
             currentRouter.setRouteType(Router.RouteType.REDIRECT);
         } catch (LibraryServiceException e) {
+            user.setBanned(getReversedUserBannedStatus(user.getBanned()));
             setErrorMessage(request, e.getMessage());
             currentRouter.setPagePath(PageStorage.HOME);
             currentRouter.setRouteType(Router.RouteType.FORWARD);
