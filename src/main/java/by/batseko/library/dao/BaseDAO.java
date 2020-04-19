e8e37b3b9fdd1ee3c67eaf861b47b75d0d0c2e0e
Dmitry Batseko <studytemp@hotmail.com>
2020-04-19 21:06:08 +0300

    Delete closeStatement method

--- src/main/java/by/batseko/library/dao/BaseDAO.java
+++ src/main/java/by/batseko/library/dao/BaseDAO.java
@@ -5,20 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public abstract class BaseDAO {
     private static final Logger LOGGER = LogManager.getLogger(BaseDAO.class);
-
-    protected void closeStatement(Statement statement) {
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                LOGGER.error("Can`t close PreparedStatement", e);
-            }
-        }
-    }
 
     protected void closeResultSet(ResultSet resultSet) {
         if (resultSet != null) {
