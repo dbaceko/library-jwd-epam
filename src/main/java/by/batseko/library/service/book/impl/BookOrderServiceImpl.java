c2585d607be9d0010714a4fbdb3870ad0f523c31
Dmitry Batseko <studytemp@hotmail.com>
2020-04-28 02:13:12 +0300

    Improve addBookOrder method availableInstancesEndedException initialised only twice in worst case

--- src/main/java/by/batseko/library/service/book/impl/BookOrderServiceImpl.java
+++ src/main/java/by/batseko/library/service/book/impl/BookOrderServiceImpl.java
@@ -8,10 +8,14 @@ import by.batseko.library.exception.LibraryDAOException;
 import by.batseko.library.exception.LibraryServiceException;
 import by.batseko.library.factory.DAOFactory;
 import by.batseko.library.service.book.BookOrderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class BookOrderServiceImpl implements BookOrderService {
+    private static final Logger LOGGER = LogManager.getLogger(BookOrderServiceImpl.class);
+
     private static final BookOrderDAO bookOrderDAO = DAOFactory.getInstance().getBookOrderDAO();
     private static final BookInstanceDAO bookInstanceDAO = DAOFactory.getInstance().getBookInstanceDAO();
 
@@ -24,13 +28,18 @@ public class BookOrderServiceImpl implements BookOrderService {
             throw new LibraryServiceException(e.getMessage(), e);
         }
         LibraryServiceException availableInstancesEndedException = new LibraryServiceException("query.bookInstance.find.empty");
-        for (String bookInstanceUUID : availableBookInstanceUUIDs) {
+        for (int i = 0; i < availableBookInstanceUUIDs.size(); i++) {
             try {
-                bookOrder.getBookInstance().setUuid(bookInstanceUUID);
+                bookOrder.getBookInstance().setUuid(availableBookInstanceUUIDs.get(i));
                 bookOrderDAO.addBookOrder(bookOrder);
                 return;
             } catch (LibraryDAOException e) {
-                availableInstancesEndedException = new LibraryServiceException(e.getMessage(), e);
+                if (i == availableBookInstanceUUIDs.size() - 1) {
+                    LOGGER.warn(String.format("All books %s is already booked", bookOrder.getBookInstance().getBook()));
+                    availableInstancesEndedException = new LibraryServiceException(e.getMessage(), e);
+                } else {
+                    LOGGER.info(String.format("Book instance %s is already booked", bookOrder.getBookInstance()));
+                }
             }
         }
         throw availableInstancesEndedException;
